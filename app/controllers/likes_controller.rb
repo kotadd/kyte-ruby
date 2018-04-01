45c08052e376398f5935dfe16e021fa7f715dd66
Kota Nishinaka <private2030kota@gmail.com>
2018-04-01 16:03:12 +0900

    style: button to icon

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,12 +5,14 @@ class LikesController < ApplicationController
     @like = Like.new(user_id: @current_user.id, post_id: params[:post_id])
     @like.save
     # @likes_count = Like.where(post_id: params[:post_id]).count
+    flash[:notice] = "お気に入りに登録しました"
     redirect_to("/posts/#{params[:post_id]}")
   end
 
   def destroy
     @like = Like.find_by(user_id: @current_user.id, post_id: params[:post_id])
     @like.destroy
+    flash[:notice] = "お気に入りから削除しました"
     # @likes_count = Like.where(post_id: params[:post_id]).count
     redirect_to("/posts/#{params[:post_id]}")
   end
@@ -18,12 +20,14 @@ class LikesController < ApplicationController
   def create_index
     @like = Like.new(user_id: @current_user.id, post_id: params[:post_id])
     @like.save
+    flash[:notice] = "お気に入りに登録しました"
     redirect_to("/posts/index")
   end
 
   def destroy_index
     @like = Like.find_by(user_id: @current_user.id, post_id: params[:post_id])
     @like.destroy
+    flash[:notice] = "お気に入りから削除しました"
     redirect_to("/posts/index")
   end
   
